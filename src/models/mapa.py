14808735b11f8e8f8906395526c22ea7e75a7abb
Leymar <buenaventuraleymar901@gmail.com>
2024-11-18 18:00:53 -0500

    se creo la clase mapa

---
+++ src/models/mapa.py
@@ -0,0 +1,61 @@
+import pygame
+import sys
+
+
+class Mapa:
+    def __init__(self, ancho, alto, fondo, tienda_img, recompensa_img, vidas_img):
+        """
+        Clase para configurar y renderizar el mapa.
+        :param ancho: Ancho de la ventana
+        :param alto: Alto de la ventana
+        :param fondo: Ruta de la imagen de fondo
+        :param tienda_img: Ruta de la imagen de la tienda
+        :param recompensa_img: Ruta de la imagen del mostrador de recompensa
+        :param vidas_img: Ruta de la imagen del mostrador de vidas
+        """
+        self.ancho = ancho
+        self.alto = alto
+        try:
+            # Fondo
+            self.fondo = pygame.image.load(fondo)
+            self.fondo = pygame.transform.scale(self.fondo, (ancho, alto))
+
+            # Tienda
+            self.tienda = pygame.image.load(tienda_img)
+            self.tienda = pygame.transform.scale(
+                self.tienda, (150, 150))  # Ajustar tamaño
+
+            # Mostrador de recompensa
+            self.recompensa = pygame.image.load(recompensa_img)
+            self.recompensa = pygame.transform.scale(
+                self.recompensa, (70, 50))  # Ajustar tamaño
+
+            # Mostrador de vidas
+            self.vidas = pygame.image.load(vidas_img)
+            self.vidas = pygame.transform.scale(
+                self.vidas, (50, 50))  # Ajustar tamaño
+
+        except pygame.error as e:
+            print(f"Error al cargar las imágenes: {e}")
+            sys.exit()
+
+    def configurar_mapa(self):
+        # Inicialización de Pygame
+        pygame.init()
+
+        # Configuración de la ventana
+        self.screen = pygame.display.set_mode((self.ancho, self.alto))
+        pygame.display.set_caption("Mapa con Objetos")
+
+    def dibujar_mapa(self):
+        # Dibujar el fondo
+        self.screen.blit(self.fondo, (0, 0))
+
+        # Dibujar la tienda (esquina inferior izquierda)
+        self.screen.blit(self.tienda, (0, 660))
+
+        # Dibujar el mostrador de recompensa (alineado en la parte inferior)
+        self.screen.blit(self.recompensa, (2, 10))
+
+        # Dibujar el mostrador de vidas (alineado en la parte inferior)
+        self.screen.blit(self.vidas, (1160, 755))
